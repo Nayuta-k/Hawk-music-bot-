279a88061e68b0e17486429b36ddd08f31293f11
Nayuta-k <43514681+Nayuta-k@users.noreply.github.com>
2018-09-24 11:30:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core');//need this package to make the bot join channel and play music
 const streamOptions = {seek: 0, volume: 1};
-const config = require('./modules/config.json')
+const config = require('./modules/config.json')//contain the prefix, your id, and the token 
 const active = new Map();
 
 var prefix = (config.prefix);
 
-console.log('connexion.....')
+console.log('connexion.....')//not needed but usefull to make sure the bot is connecting
 
 client.on('ready', () => {
-    client.user.setPresence({ game : { name : `${client.guilds.size} servers`, url : 'https://www.twitch.tv/monstercat'}});
+    client.user.setPresence({ game : { name : `${client.guilds.size} servers`, url : 'https://www.twitch.tv/monstercat'}});//the bot status will be in streaming you already can change that 
     //client.user.setPresence({ game : { name : 'Access denied', url : 'https://www.twitch.tv/monstercat'}})
     console.log('connected !')
 });
@@ -42,8 +42,8 @@ client.login(process.env.TOKEN); //protect your token if you use a vps, else use
 
 client.on('message', message => {
 var msg = message.content.toLowerCase();
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);//usefull cause you can make avatar, kick, ban command
+//const command = args.shift().toLowerCase(); //not needed in our code
 const username = message.author.username;
 
 
@@ -53,7 +53,7 @@ const username = message.author.username;
     if(message.content.indexOf(config.prefix) !== 0) return
 
     try {
-        let commandFile = require(`./command/${command}.js`);
+        let commandFile = require(`./command/${command}.js`);//your command will be in the file link not in the code 
         commandFile.run(Discord, client, message, args, ytdl, streamOptions, username)
     } catch (err) {
         return 
